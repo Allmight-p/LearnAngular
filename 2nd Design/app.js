103805d90066a5e2e705810e0782bcbfdb1d815b
Allmight-p <imnaveene@gmail.com>
2021-01-14 07:48:36 +0500

    NameCalcPro

---
+++ 2nd Design/app.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+  angular.module("NameCalc", [])
+  .controller("NameCalcCont", function($scope) {
+    $scope.name ="";
+    $scope.totalvalue = 0;
+
+    $scope.Detectnoofstring = function() {
+        var totalamount =Findvalueforstring($scope.name)
+        $scope.totalvalue = totalamount;
+    };
+    function Findvalueforstring(string) {
+        var totalvalueofstring = 0;
+        for(var i=0;i < string.length;i++){
+            totalvalueofstring = totalvalueofstring + charCodeAt(i);
+        }
+        return totalvalueofstring;
+    }
+  });
+
+
+}
+)();
